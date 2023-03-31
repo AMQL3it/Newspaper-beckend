3cab30d5e9c9252862a3db8d78312d9375a6b190
AMQL3it <122715661+AMQL3it@users.noreply.github.com>
2023-03-31 17:35:08 +0600

    second commit

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -5,9 +5,17 @@ const {
     addMemberValidators,
     addMemberValidatorHandler
 } = require('../middlewares/members/memberValidators');
+const newspaperController = require('../controllers/newspaperController');
 
 const router = express.Router();
 
 router.post('/register', avaterUpload, addMemberValidators, addMemberValidatorHandler, createMember);
+
+// Newspapers routes
+router.post('/papers', newspaperController.createNewspaper);
+router.put('/papers/:paper_id', newspaperController.updateNewspaper);
+router.delete('/papers/:paper_id', newspaperController.deleteNewspaper);
+router.get('/papers', newspaperController.getNewspapers);
+router.get('/papers/:paper_id', newspaperController.getNewspaperById);
 
 module.exports = router;
